2418d67c018dff15ee1e0f448818071744a20227
Aliyv019 <tahiraliyev2006@gmail.com>
2024-07-23 16:43:26 +0400

    added touch sliding

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const img_div=document.querySelector('div')
-const imgs=img_div.querySelectorAll('img')
+
 const left_arrow=document.querySelectorAll('button')[0]
 const right_arrow=document.querySelectorAll('button')[1]
 let activeimgs=[0,1,2]
-
+const imgsreload=(activeimgs)=>{
+    img_div.innerHTML=`
+        <img src="./img/img (${activeimgs[0]}).png">
+        <img src="./img/img (${activeimgs[1]}).png">
+        <img src="./img/img (${activeimgs[2]}).png">`
+    const imgs=img_div.querySelectorAll('img')
+}
+const previmgs=()=>{
+    if(activeimgs[2]==0){
+        activeimgs[0]-=1
+        activeimgs[1]-=1
+        activeimgs[2]=8
+    }
+    else if(activeimgs[1]==0){
+        activeimgs[0]-=1
+        activeimgs[1]=8
+        activeimgs[2]-=1
+    }
+    else if(activeimgs[0]==0){
+        activeimgs[0]=8
+        activeimgs[1]-=1
+        activeimgs[2]-=1
+    }
+    else{
+        activeimgs[0]-=1
+        activeimgs[1]-=1
+        activeimgs[2]-=1
+    }
+    console.log(activeimgs);
+    imgsreload(activeimgs)
+}
 const nextimgs=()=>{
     if(activeimgs[2]==8){
         activeimgs[0]+=1
@@ -28,39 +58,33 @@ const nextimgs=()=>{
     console.log(activeimgs);
     imgsreload(activeimgs)
 }
-const imgsreload=(activeimgs)=>{
-        img_div.innerHTML=`
-            <img src="./img/img (${activeimgs[0]}).png">
-            <img src="./img/img (${activeimgs[1]}).png">
-            <img src="./img/img (${activeimgs[2]}).png">`
-}
+
 imgsreload(activeimgs)
 right_arrow.addEventListener('click',nextimgs)
-left_arrow.addEventListener('click',()=>{
-    if(activeimgs[2]==0){
-        activeimgs[0]-=1
-        activeimgs[1]-=1
-        activeimgs[2]=8
+left_arrow.addEventListener('click',previmgs)
+// setInterval(nextimgs,2000)
+
+let startX = 0, endX = 0;
+
+const handleTouchStart = (e) => {
+    startX = e.touches ? e.touches[0].clientX : e.clientX;
+}
+
+const handleTouchMove = (e) => {
+    endX = e.touches ? e.touches[0].clientX : e.clientX;
+}
+
+const handleTouchEnd = () => {
+    if (startX > endX) {
+        nextimgs();
+    } else if (startX < endX) {
+        previmgs();
     }
-    else if(activeimgs[1]==0){
-        activeimgs[0]-=1
-        activeimgs[1]=8
-        activeimgs[2]-=1
-    }
-    else if(activeimgs[0]==0){
-        activeimgs[0]=8
-        activeimgs[1]-=1
-        activeimgs[2]-=1
-    }
-    else{
-        activeimgs[0]-=1
-        activeimgs[1]-=1
-        activeimgs[2]-=1
-    }
-    // for(let j of activeimgs){
-        //     imgs[j].style.display="block"
-        // }
-    console.log(activeimgs);
-    imgsreload(activeimgs)
-})
-setInterval(nextimgs,3000)
+}
+
+img_div.addEventListener('touchstart', handleTouchStart);
+img_div.addEventListener('touchmove', handleTouchMove);
+img_div.addEventListener('touchend', handleTouchEnd);
+img_div.addEventListener('mousedown', handleTouchStart);
+img_div.addEventListener('mousemove', handleTouchMove);
+img_div.addEventListener('mouseup', handleTouchEnd);
